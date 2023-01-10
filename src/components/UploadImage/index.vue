cdb08f76fc3dfb9cdb306354662c2f52502bafe4
LCT <1044588442@qq.com>
2023-01-10 12:36:34 +0800

    弹窗组件排版2

--- src/components/UploadImage/index.vue
+++ src/components/UploadImage/index.vue
@@ -1,27 +1,57 @@
 <template>
-    <el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card"
-        :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
-        <i class="el-icon-plus"></i>
-    </el-upload>
-    <el-dialog :visible.sync="dialogVisible">
-        <img width="100%" :src="dialogImageUrl" alt="">
-    </el-dialog>
+    <div>
+        <el-upload action="#" list-type="picture-card" :on-preview="preview" :on-remove="remove" :limit="1"
+            :file-list="fileList" :http-request="customUpload">
+            <i class="el-icon-plus" />
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
+    </div>
 </template>
 <script>
+var COS = require('cos-js-sdk-v5')
+const cos = new COS({
+    SecretId: 'AKIDnTwBD0bz82LQJzPbZLwKURB3kETUGqDL',
+    SecretKey: 'yvgFcmkZJzYw7C4jCSUHrEMM9i4B4kKg'
+})
+
 export default {
     data() {
         return {
             dialogImageUrl: '',
-            dialogVisible: false
-        };
+            dialogVisible: false,
+            fileList: []
+        }
     },
     methods: {
-        handleRemove(file, fileList) {
-            console.log(file, fileList);
+        preview() {
+            this.dialogImageUrl = this.fileList[0].url
+            this.dialogVisible = true
         },
-        handlePictureCardPreview(file) {
-            this.dialogImageUrl = file.url;
-            this.dialogVisible = true;
+        remove() {
+            this.fileList = []
+        },
+
+        // 上传图片至腾讯云
+        customUpload({ file }) {
+            cos.uploadFile({
+                Bucket: 'hm85lct-1316362757', /* 填写自己的 bucket，必须字段 */
+                Region: 'ap-guangzhou', /* 存储桶所在地域，必须字段 */
+                Key: file.name, /* 存储在桶里的对象键（例如:1.jpg，a/b/test.txt，图片.jpg）支持中文，必须字段 */
+                Body: file, // 上传文件对象
+                SliceSize: 1024 * 1024 * 5, /* 触发分块上传的阈值，超过5MB使用分块上传，小于5MB使用简单上传。可自行设置，非必须 */
+                onProgress: function (progressData) {
+                    console.log(JSON.stringify(progressData))
+                }
+            }, (err, data) => {
+                if (err) {
+                    console.log('上传失败', err)
+                } else {
+                    console.log('上传成功')
+                    this.fileList = [{ url: `http://${data.Location}` }]
+                }
+            })
         }
     }
 
--- src/views/category/add-category/index.vue
+++ src/views/category/add-category/index.vue
@@ -1,20 +1,20 @@
 <template>
     <el-dialog title="添加分类" :visible="visible" @close="closeFn">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
             <el-form-item label="活动名称" prop="name">
-                <el-input v-model="ruleForm.name"></el-input>
+                <el-input v-model="ruleForm.name" />
             </el-form-item>
             <el-form-item label="活动名称" prop="name">
-                <el-input v-model="ruleForm.name"></el-input>
+                <el-input v-model="ruleForm.name" />
             </el-form-item>
             <el-form-item label="活动名称" prop="name">
-                <el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card"
-                    :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
-                    <i class="el-icon-plus"></i>
-                </el-upload>
-                <el-input v-model="ruleForm.name" placeholder="强制设置图片跳过上传"></el-input>
+                <upload-image /> <!-- 上传图片组件 -->
+                <el-input v-model="ruleForm.name" placeholder="强制设置图片跳过上传" />
             </el-form-item>
-
+            <el-form-item class="button-style">
+                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                <el-button @click="resetForm('ruleForm')">重置</el-button>
+            </el-form-item>
         </el-form>
     </el-dialog>
 </template>
@@ -30,17 +30,32 @@ export default {
                 name: [
                     { required: true, message: '请输入活动名称', trigger: 'blur' },
                     { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-                ],
+                ]
             }
         }
     },
     methods: {
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('submit!')
+                } else {
+                    console.log('error submit!!')
+                    return false
+                }
+            })
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields()
+        },
         closeFn() {
             this.$emit('close')
         }
     }
 }
 </script>
-<style>
-
+<style scoped>
+.button-style {
+    text-align: center;
+}
 </style>
