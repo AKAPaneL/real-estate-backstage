67d728192dd7aa5e73cdfc64af91914ce254bac7
Pane.L <12314389+Whatssup-PaneL@user.noreply.gitee.com>
2023-01-12 11:29:11 +0800

    分类模块接口修改

--- src/api/category.js
+++ src/api/category.js
@@ -1,8 +1,9 @@
 import request from '@/utils/request'
 
 // 封装获取分类列表
-export function getCategory(data) {
-  return request.get('/categories', { data })
+export function getCategory(params) {
+  console.log(params)
+  return request.get('/categories', { params })
 }
 // 封装获取分类数量
 export function getCategoryCount(data) {
